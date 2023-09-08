3795c3af51a6da82c484c71b412209e0fb8f5bfb
Naoya Furudono <naoya-furudono@pepabo.com>
2023-09-08 15:31:57 +0900

    やってみた

---
+++ try_flask/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_name(name):
+    return render_template('hello.html', name=name)
